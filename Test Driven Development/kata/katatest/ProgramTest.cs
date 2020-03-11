0217f8c8aa9d2d2f4fa9ee831e17c9759dfcc3e0
Guy Cluet <cluet.guy@gmail.com>
2020-03-11 20:42:48 +0100

    Add katatest project

---
+++ Test Driven Development/kata/katatest/ProgramTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace katatest
+{
+    [TestClass]
+    public class ProgramTest
+    {
+      
+    }
+}
---
+++ Test Driven Development/kata/katatest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("katatest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("katatest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d660b7dc-1f9f-424c-8582-e208f472b240")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
